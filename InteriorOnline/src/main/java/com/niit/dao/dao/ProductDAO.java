57c9b36ddf44ded4a59335dfb7725831601ec884
VarshaVanaraj <varsharajvanaraj@gmail.com>
2016-10-15 18:36:10 +0530

    Add files via upload

---
+++ InteriorOnline/src/main/java/com/niit/dao/dao/ProductDAO.java
@@ -0,0 +1,12 @@
+package com.niit.dao;
+
+import java.util.List;
+
+import com.niit.model.Product;
+
+public interface ProductDAO {
+	public void addProduct(Product product);
+	public void updateProduct(Product product);
+	public void deleteProduct(Product product);
+	public List<Product> viewProduct();
+}
